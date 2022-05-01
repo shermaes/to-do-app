17e2dd2b8f01fb8a6e624b98795803def7ca43eb
Sherilyn Maestre <you@example.com>
2022-05-01 11:39:06 -0500

    added the reducer function

---
+++ src/components/Reducer.jsx
@@ -0,0 +1,14 @@
+function reducer(state, action) {
+    switch (action.type) {
+        case 'add-note':
+            return state;
+    
+        case 'remove-note':
+            return state;
+
+        case 'update-note':
+            return state    
+    }
+}
+
+export default reducer
